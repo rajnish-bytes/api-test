7f70fe87f9ee14f6aa2d7edb870eeefcc60dc92a
Rajnish <razz.king2004@gmail.com>
2024-03-15 13:11:41 +0530

    Test api by axios

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { assert, beforeAll, describe, expect, expectTypeOf, test } from 'vitest'
 
 // New user data type
@@ -15,36 +16,26 @@ interface Data {
 let body: Data; // Register user data 
 
 /** Register new user */
-describe("user registerd Api test", () => {
+describe("user registerd Api test", async () => {
       let response: any; // Api response
 
       /** Call before all test */
-      beforeAll(async () => {
-            response = await fetch('https://dummyjson.com/auth/login', {
-                  method: 'POST',
-                  headers: { 'Content-Type': 'application/json' },
-                  body: JSON.stringify({
+      response = await axios.post('https://dummyjson.com/auth/login', {
+            username: 'atuny0',
+            password: '9uQFF1Lh',
+      });
+      body = response.data
+      console.log(body)
 
-                        username: 'atuny0',
-                        password: '9uQFF1Lh',
-
-                  })
-
-            })
-            body = await response.json();
-            console.log(body)
-      }, 10000)
 
       /** Check Api response*/
       test('should have response status 200', () => {
             expect.soft(response.status).toBe(200)
-            console.log(body)
       })
 
       /** Check data type of body */
       test('type of response data', () => {
             expectTypeOf(body).toBeArray
-            console.log(body)
       })
 
       /** check register username is correct or not in response body */
@@ -60,21 +51,18 @@ describe("user registerd Api test", () => {
 
 /**
  * Login Current user 
- * */ 
-describe("current user login Api test", () => {
+ * */
+describe("current user login Api test", async () => {
       let response: any;
       let userData: any;
-      beforeAll(async () => {
-            response = await fetch('https://dummyjson.com/auth/me', {
-                  method: 'GET',
-                  headers: {
-                        'Authorization': body.token
-                  },
+      response = await axios.get('https://dummyjson.com/auth/me', {
+            headers: {
+                  Authorization: body.token,
+            },
+      });
 
-            })
-            userData = await response.json()
-            console.log(userData)
-      }, 10000)
+      userData = response.data;
+      console.log(userData)
 
       /** Check login user name is valid or not */
       test('User login', () => {
@@ -85,13 +73,11 @@ describe("current user login Api test", () => {
       /** Check Api response*/
       test('should have response status 200', () => {
             expect.soft(response.status).toBe(200)
-            console.log(userData)
       })
 
       /** Check data type of login user data */
       test('type of response data', () => {
             expectTypeOf(userData).toBeArray
-            console.log(userData)
       })
 
       /** Check username or password should be correct*/
